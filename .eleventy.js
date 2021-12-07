3b445075bd510622655cd3c76cc0682b919019be
Fabian <fabiandiazbarboza@gmail.com>
2021-12-07 12:49:25 -0600

    cambio5

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
 module.exports = function(eleventyConfig){
+    eleventyConfig.addPassthroughCopy('./src/estilospagina.css')
+    eleventyConfig.addPassthroughCopy('./src/interacciones.js') 
+    eleventyConfig.addPassthroughCopy('./src/imagenes') 
+    
+
+
     return {
         dir: {
         input: "src"
